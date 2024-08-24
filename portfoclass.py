955126324d506f181d4cc9b36507760739a30ad9
Ahad Salim <ahadsalim@yahoo.com>
2024-08-24 13:09:23 +0330

    Update portfoclass.py

--- portfoclass.py
+++ portfoclass.py
@@ -1,5 +1,17 @@
 import MetaTrader5 as mt5
 import myforexclass as mf
 
-class portfo_forex(forex_backtest_class) :
-    pass    
+class portfo_forex() :
+    def __repr__(self): 
+        '''
+        Present the Class
+        '''
+        return "Forex (start={} , end={} , interval={} )".format(self.start,self.end, self.interval)
+    
+    def __init__ (self ,tickers, period):
+        self.tickers = tickers
+        self.period = period
+    
+    
+    def mean_param_sma(self , ticker , period):
+        
