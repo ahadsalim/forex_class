e4f54322183be3104a130f409f4fa80aff3ee1d0
Ahad Salim <ahadsalim@yahoo.com>
2024-10-03 12:00:35 +0330

    update

--- update_tickers.py
+++
@@ -1,7 +0,0 @@
-import Coinex_API_Class as CoinexAPI
-    
-api_key = "47D7C4B286224298BB3D88A9D7161A45"
-api_secret = "F0F2D4156F6892ED2F03576FEE74CFA658C5F8271DCE102D"
-coinex = CoinexAPI.Coinex_API(api_key,api_secret)
-
-coinex.save_filtered_spot_market()
