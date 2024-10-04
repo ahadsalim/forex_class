26edd5cacd94ee1011eb9e55272502f194ae7e7e
Ahad Salim <ahadsalim@yahoo.com>
2024-10-04 19:30:05 +0330

    Update Coinex_API_Class.py

--- Coinex_API_Class.py
+++ Coinex_API_Class.py
@@ -6,6 +6,7 @@ import requests
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
+from typing import Union
 from tradingview_ta import TA_Handler, Interval, Exchange , TradingView
 from urllib.parse import urlparse
 
@@ -98,12 +99,14 @@ class Coinex_API(object):
             raise ValueError(response.text)
         return response
     
-    def get_spot_market(self,ticker=""):
+    def get_spot_market(self, ticker: str = "") -> Union[pd.DataFrame, dict]:
         '''
             Get information about ticker in spot market in CoinEx site
-            Arguman :
-                null : Get all tickers in exchange Return in Dataframe format
-                ticker : To get detail of ticker in exchange like : BTCUSDT  Retun in json format
+            Args:
+                ticker (str): Optional, to get detail of ticker in exchange like : BTCUSDT. Default is "".
+            Returns:
+                Union[pd.DataFrame, dict]: If ticker is "" then it returns all tickers in exchange in DataFrame format.
+                                            If ticker is not "" then it returns detail of ticker in exchange in dict format.
         '''
         request_path = "/spot/market"
         params = {"market": ticker}
@@ -138,15 +141,23 @@ class Coinex_API(object):
             return res["data"]
         else :
             raise ValueError(res["message"])
-
     def save_filtered_spot_market (self,min_price):
         '''
             This function updates the symbols information that price is upper than min_price every 24 hours and saves it in the tickers.csv file.
+            It only selects the symbols that end with 'USDT' and uses the last price to filter the symbols.
+            It also drops the columns that are not necessary for the analysis.
+            It updates the price, value, volume_sell, volume_buy columns for each symbol.
+            It then filters the symbols with price above min_price.
+            Finally, it saves the DataFrame to the tickers.csv file.
         '''
         df = self.get_spot_market()
+        # Select only the symbols that end with 'USDT'
         data = df[df['market'].str.endswith('USDT')].copy()
+        # Drop the columns that are not necessary for the analysis
         data.drop(columns=["base_ccy","base_ccy_precision","quote_ccy","quote_ccy_precision"] , inplace=True)
+        # Reindex the columns to match the order of the columns in the DataFrame
         data = data.reindex(columns=['market','min_amount','maker_fee_rate','taker_fee_rate','is_amm_available','is_margin_available'])
+        # Iterate over each symbol and update the price, value, volume_sell, volume_buy columns
         for index,symbol in tqdm(data.iterrows() ,total=len(data)) :
             try :
                 info = self.get_spot_price_ticker(symbol['market'])[0]
@@ -160,35 +171,50 @@ class Coinex_API(object):
                 data.loc[index,"value"]= 0
                 data.loc[index,"volume_sell"]= 0
                 data.loc[index,"volume_buy"]= 0
-
+        # Filter the symbols with price above min_price
         df= data[(data['price'].astype(float)> min_price)]
         try :
+            # Save the DataFrame to the tickers.csv file
             df.to_csv("tickers.csv" , index=False)
             return True
         except Exception as e:
             print("Can't save data !!!" , e)
             return e
 
-    def get_spot_kline(self,ticker,period,limit):
-        '''
-            ticker : ticker name
-            limit : Number of transaction data items. Default as 100, max. value 1000
-            period :One of ["1min", "3min", "5min", "15min", "30min", "1hour", "2hour", "4hour", "6hour", "12hour", "1day", "3day", "1week"]
+    def get_spot_kline(self, ticker, period, limit):
+        """
+        Get the last {limit} kline data of {ticker} in {period} period
 
-            return : DataFrame
-        '''
+        Args:
+            ticker (str): Ticker name
+            period (str): One of ["1min", "3min", "5min", "15min", "30min", "1hour", "2hour", "4hour", "6hour", "12hour", "1day", "3day", "1week"]
+            limit (int): Number of transaction data items. Default as 100, max. value 1000
+
+        Returns:
+            pd.DataFrame: DataFrame with the following columns:
+                Time (datetime64[ns]): Time of the kline data
+                Open (float): Open price of the kline data
+                High (float): High price of the kline data
+                Low (float): Low price of the kline data
+                Close (float): Close price of the kline data
+                Return (float): Return of the kline data
+                Cum_Return (float): Cumulative return of the kline data
+                Volume (float): Volume of the kline data
+                Value (float): Value of the kline data
+                Cum_Value (float): Cumulative value of the kline data
+        """
         request_path = "/spot/kline"
-        params = {"market": ticker , "period" : period , "limit": limit }
+        params = {"market": ticker, "period": period, "limit": limit}
         response = self.request(
             "GET",
             "{url}{request_path}".format(url=self.url, request_path=request_path),
             params=params,
         )
-        res=response.json()
-        df2= pd.DataFrame()
-        if res["code"]==0 :
+        res = response.json()
+        df2 = pd.DataFrame()
+        if res["code"] == 0:
             df = pd.json_normalize(res["data"])
-            if df.shape[0] != 0 :  # If there is any data for this ticker
+            if df.shape[0] != 0:  # If there is any data for this ticker
                 df2['Time'] = pd.to_datetime(df['created_at'], unit='ms')
                 df2['Open'] = df["open"].astype(float)
                 df2['High'] = df["high"].astype(float)
@@ -198,40 +224,84 @@ class Coinex_API(object):
                 df2["Cum_Return"] = np.exp(df2["Return"].cumsum())
                 df2['Volume'] = df["volume"].astype(float)
                 df2['Value'] = df["value"].astype(float)
-                df2['Cum_Value']= df2['Value'].cumsum()
+                df2['Cum_Value'] = df2['Value'].cumsum()
                 return df2
-            else :
+            else:
                 return False
-        else :
+        else:
             raise ValueError(res["message"])
 
-    def get_cum_ret_filtered_tickers(self,period,limit) :
-        '''
-            Get Cumulative Return in the period in limit time for all tickers in tickers.csv
-            tradingview : check tradingview site 
-        '''
+    def get_cum_ret_filtered_tickers(self, period: str, limit: int) -> pd.DataFrame:
+        """
+        Get Cumulative Return in the period in limit time for all tickers in tickers.csv
+
+        :param period: One of ["1min", "3min", "5min", "15min", "30min", "1hour", "2hour", "4hour", "6hour", "12hour", "1day", "3day", "1week"]
+        :param limit: Number of transaction data items. Default as 100, max. value 1000
+        :return: pd.DataFrame with the following columns:
+            Time (datetime64[ns]): Time of the kline data
+            symbol (str): Symbol of the ticker
+            min_amount (float): Minimum amount of the ticker
+            maker_fee_rate (float): Maker fee rate of the ticker
+            taker_fee_rate (float): Taker fee rate of the ticker
+            Close (float): Close price of the kline data
+            Return (float): Return of the kline data
+            Cum_Return (float): Cumulative return of the kline data
+            Volume (float): Volume of the kline data
+            Value (float): Value of the kline data
+            Cum_Value (float): Cumulative value of the kline data
+            period (str): Period of the kline data
+            limit (int): Limit of the kline data
+        """
         markets = pd.read_csv("tickers.csv")
-        df=pd.DataFrame(columns=['Time',"symbol","min_amount","maker_fee_rate","taker_fee_rate",
-                                 'Close','Return','Cum_Return','Volume','Value','Cum_Value','period','limit'])
-        for index,symbol in tqdm(markets.iterrows() , total=len(markets)) :
-            ticker= symbol['market']
-            try :
-                data=self.get_spot_kline(ticker,period,limit).iloc[-1]
-                info ={"Time" : [data["Time"]] , "symbol": [ticker] , "min_amount": [symbol["min_amount"]] , 
-                       "maker_fee_rate": [symbol["maker_fee_rate"]] ,"taker_fee_rate": [symbol["taker_fee_rate"]],
-                       "Close": [data['Close']], "Return": [data['Return']], "Cum_Return":[data["Cum_Return"]] ,
-                       "Volume": [data['Volume']] ,"Value": [data["Value"]] ,"Cum_Value": [data['Cum_Value']] , "period":[period] , "limit":[limit] }
+        df = pd.DataFrame(columns=['Time', "symbol", "min_amount", "maker_fee_rate", "taker_fee_rate",
+                                   'Close', 'Return', 'Cum_Return', 'Volume', 'Value', 'Cum_Value', 'period', 'limit'])
+        for index, symbol in tqdm(markets.iterrows(), total=len(markets)):
+            ticker = symbol['market']
+            try:
+                data = self.get_spot_kline(ticker, period, limit).iloc[-1]
+                info = {"Time": [data["Time"]], "symbol": [ticker], "min_amount": [symbol["min_amount"]],
+                        "maker_fee_rate": [symbol["maker_fee_rate"]], "taker_fee_rate": [symbol["taker_fee_rate"]],
+                        "Close": [data['Close']], "Return": [data['Return']], "Cum_Return": [data["Cum_Return"]],
+                        "Volume": [data['Volume']], "Value": [data["Value"]], "Cum_Value": [data['Cum_Value']],
+                        "period": [period], "limit": [limit]}
                 info = pd.DataFrame(info)
-                if df.empty :
-                    df =info.copy()
-                else :
-                    df = pd.concat([df, info],ignore_index=True)
-            except Exception as e :
+                if df.empty:
+                    df = info.copy()
+                else:
+                    df = pd.concat([df, info], ignore_index=True)
+            except Exception as e:
                 print(e)
                 continue
         return df
     
     def get_ta_filtered_tickers (self,period,limit) :
+        """
+        Get Technical Analysis indicators for all tickers in tickers.csv in the given period and limit
+
+        Args:
+            period (str): One of ["1min", "5min", "15min", "30min", "1hour", "2hour", "4hour", "6hour", "12hour", "1day", "3day", "1week"]
+            limit (int): Number of transaction data items. Default as 100, max. value 1000
+
+        Returns:
+            pd.DataFrame: DataFrame with the following columns:
+                Time (datetime64[ns]): Time of the kline data
+                symbol (str): Symbol of the ticker
+                min_amount (float): Minimum amount of the ticker
+                maker_fee_rate (float): Maker fee rate of the ticker
+                taker_fee_rate (float): Taker fee rate of the ticker
+                Close (float): Close price of the kline data
+                Return (float): Return of the kline data
+                Cum_Return (float): Cumulative return of the kline data
+                Volume (float): Volume of the kline data
+                Value (float): Value of the kline data
+                Cum_Value (float): Cumulative value of the kline data
+                period (str): Period of the kline data
+                limit (int): Limit of the kline data
+                Recomandation (str): Recommendation of the technical analysis indicators
+                Buy (int): Buy signal of the technical analysis indicators
+                Sell (int): Sell signal of the technical analysis indicators
+                Neutral (int): Neutral signal of the technical analysis indicators
+        """
         data_spot = self.get_cum_ret_filtered_tickers(period,limit)
         data_spot['Recomandation'] = None
         data_spot['Buy'] = None
@@ -302,26 +372,39 @@ class Coinex_API(object):
         else :
             raise ValueError(res["message"])
         
-    def put_spot_order(self,ticker,side,order_type,amount,price,is_hide=False) :
-        '''
-        ticker : name of ticker
-        side : buy / sell
-        order_type : 
-            limit: Limit order (Always Valid, Good Till Cancel)
-            market: Market order
-            maker_only: Maker only order (Post only order)
-            ioc: Immediate or Cancel
-            fok: Fill or Kill
-        amount : Order amount
-        price :  Order price
-        is_hide :  if True it will be hidden in the public depth information
-        '''
+    def put_spot_order(self, ticker, side, order_type, amount, price, is_hide=False):
+        """
+        Place a Spot order
+
+        Parameters
+        ----------
+        ticker : str
+            Name of the ticker
+        side : str
+            buy/sell
+        order_type : str
+            limit/market/maker_only/ioc/fok
+        amount : float
+            Order amount
+        price : float
+            Order price
+        is_hide : bool
+            If True, it will be hidden in the public depth information
+
+        Returns
+        -------
+        dict
+            Result of the API call
+        """
+        if ticker is None or side is None or order_type is None or amount is None or price is None:
+            raise ValueError("All parameters must have value")
+
         request_path = "/spot/order"
         data = {
             "market": ticker,
             "market_type": "SPOT",
             "side": side,
-            "type": type_order,
+            "type": order_type,
             "amount": amount,
             "price": price,
             "client_id": "Ahad1360",
@@ -333,12 +416,11 @@ class Coinex_API(object):
             "{url}{request_path}".format(url=self.url, request_path=request_path),
             data=data,
         )
-        res= response.json()
-        if res["code"]==0 :
+        res = response.json()
+        if res["code"] == 0:
             return res["data"]
-        else :
+        else:
             raise ValueError(res["message"])
-
     def modify_order (self,ticker,order_id,amount=None,price=None) :
         '''
             ticker : name of ticker
