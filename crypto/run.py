828caf0a0963186981e6eaf34f49adf91648f587
Ahad Salim <ahadsalim@yahoo.com>
2024-11-01 10:49:15 +0330

    Update run.py

--- crypto/run.py
+++ crypto/run.py
@@ -6,7 +6,7 @@ def run_script(script_name):
 
 if __name__ == "__main__":
     processes = []
-    for script in ["update_symbols.py", "make_portfo.py", "update_portfo.py"]:
+    for script in ["crypto/update_symbols.py", "crypto/make_portfo.py", "crypto/update_portfo.py"]:
         p = multiprocessing.Process(target=run_script, args=(script,))
         p.start()
         processes.append(p)
