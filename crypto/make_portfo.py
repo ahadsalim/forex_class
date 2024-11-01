85bd368729816a5426866ddf656f0d577a059d27
Ahad Salim <ahadsalim@yahoo.com>
2024-11-01 14:46:48 +0330

    update

--- crypto/make_portfo.py
+++ crypto/make_portfo.py
@@ -1,4 +1,5 @@
 import json
+import time
 import sqlite3
 import Coinex_API_Class as Coinex
 
@@ -12,13 +13,14 @@ try :
         loss_limit = data['loss_limit']
         client_id = data['client_id']
         interval = data['interval']
+        sleep_check = data['sleep_check']
         higher_interval = data['higher_interval']
 except Exception as e:
     print(f"Error: {e}")
 
 conn = sqlite3.connect(db_name)
 coinex = Coinex.Coinex_API(api_key, api_secret, conn)
-
-if __name__ == "__main__":
+while True :
     coinex.make_portfo(num_symbols=5, cash=35,percent_of_each_symbol=0.2, interval=interval, higher_interval=higher_interval, HMP_candles=num_candles ,client_id=client_id)
-    conn.close()
+    time.sleep(sleep_check)
+conn.close()
